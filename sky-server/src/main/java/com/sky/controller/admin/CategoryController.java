e8fc586e00a763ff6499159dbfb32c32798757d5
TTT <1794230131@qq.com>
2024-11-22 22:39:15 +0800

    新增分类业务代码开发

--- sky-server/src/main/java/com/sky/controller/admin/CategoryController.java
+++ sky-server/src/main/java/com/sky/controller/admin/CategoryController.java
@@ -1,19 +1,37 @@
 package com.sky.controller.admin;
 
+import com.sky.dto.CategoryDTO;
 import com.sky.result.Result;
+import com.sky.service.CategoryService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Api("分类相关接口")
+@Api(tags ="分类相关接口")
 @Slf4j
 @RestController
 @RequestMapping("/admin/category")
 public class CategoryController {
 
-    public Result save(){
-        return null;
+    @Autowired
+    private CategoryService categoryService;
+
+    /**
+     * 新增分类
+     * @param categoryDTO
+     * @return
+     */
+    @PostMapping
+    @ApiOperation("新增分类")
+    public Result save(@RequestBody CategoryDTO categoryDTO){
+        log.info("新增分类:{}", categoryDTO);
+        categoryService.save(categoryDTO);
+        return Result.success();
     }
 
 }
